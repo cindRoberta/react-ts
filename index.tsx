3fd8259c3d020625f855cb8950b6b3a9cb657d77
cindRoberta <pignisfrow@hotmail.com>
2020-07-26 14:56:06 -0300

    2 files modified, 3 added, 2 deleted

--- index.tsx
+++ index.tsx
@@ -1,31 +1,15 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React from "react";
+import { render } from "react-dom";
+
+import App from "./src/App.tsx";
+import "./index.css";
 
 interface AppProps { }
 interface AppState {
   name: string;
 }
 
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>, document.getElementById('root'));
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const App = () =>
+  <span>dnsjjd</span>;
+
+export default App;
